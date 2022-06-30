81944a0777ea73b823653b3123c9aac1e9b414f7
Daniel Gallardo <dgallardo.c@outlook.com>
2022-06-30 13:13:12 -0500

    Add sidebar component

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,42 @@
+import './sidebar.css';
+const defEntries = ['Inbox', 'Today', 'Upcoming', 'Anytime'];
+const usrEntries = ['Default'];
+
+function createLi(name) {
+    const li = document.createElement('li');
+    li.classList.add(name.toLowerCase());
+    li.textContent = name;
+
+    return li;
+}
+
+function sidebar() {
+    const fr = document.createDocumentFragment();
+
+    const defaultEntries = document.createElement('ul');
+    defaultEntries.classList.add('default-entries');
+
+    for (let entry of defEntries) {
+        const newLi = createLi(entry);
+        defaultEntries.appendChild(newLi);
+    }
+
+    fr.appendChild(defaultEntries);
+
+    const hr = document.createElement('hr');
+    fr.appendChild(hr);
+
+    const userEntries = document.createElement('ul');
+    userEntries.classList.add('user-entries');
+
+    for (let entry of usrEntries) {
+        const newLi = createLi(entry);
+        userEntries.appendChild(newLi);
+    }
+
+    fr.appendChild(userEntries);
+    
+    return fr;
+}
+
+export default sidebar();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './index.css';
 import header from './components/header/header.js';
+import sidebar from './components/sidebar/sidebar.js';
 
+const headerContent = document.querySelector('.header-content');
+headerContent.appendChild(header);
+const sidebarContent = document.querySelector('.sidebar');
+sidebarContent.appendChild(sidebar);
 const test = document.querySelector('.header-content');
 test.appendChild(header);
-
-body.textContent = "Hello webpack!"
