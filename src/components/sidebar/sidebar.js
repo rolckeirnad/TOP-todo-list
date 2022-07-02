e56d0350b61d99101052c6077cc972f6ebb92c55
Daniel Gallardo <dgallardo.c@outlook.com>
2022-07-01 20:11:02 -0500

    Add header to projects

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -26,6 +26,20 @@ function sidebar() {
     const hr = document.createElement('hr');
     fr.appendChild(hr);
 
+    const projectHeader = document.createElement('div');
+    projectHeader.classList.add('sidebar-projects-header');
+
+    const projectHeaderTitle = document.createElement('h2');
+    projectHeaderTitle.textContent = "Projects";
+
+    const projectHeaderButton = document.createElement('button');
+    projectHeaderButton.classList.add('sidebar-header-button');
+    projectHeaderButton.innerText = "+";
+
+    projectHeader.append(projectHeaderTitle, projectHeaderButton);
+
+    fr.appendChild(projectHeader);
+
     const userEntries = document.createElement('ul');
     userEntries.classList.add('user-entries');
 
