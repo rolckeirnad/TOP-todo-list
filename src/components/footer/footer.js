6ab42c2d217c2f82674ad0415fb11e9d9a010263
Daniel Gallardo <dgallardo.c@outlook.com>
2022-08-03 01:19:03 -0500

    Update footer content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,9 +11,27 @@ function footer() {
     aGithub.href = "https://github.com/rolckeirnad";
     aGithub.appendChild(github);
 
-    nav.appendChild(aGithub);
+    const pixabay = document.createElement('a');
+    pixabay.href = "https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=146642";
+    pixabay.append('Pixabay');
 
-    fr.appendChild(nav);
+    const user = document.createElement('a');
+    user.href = "https://pixabay.com/es/users/openclipart-vectors-30363/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=146642"
+    user.append('OpenClipart-Vectors');
+
+    const link1 = document.createElement('span');
+    link1.append('Image from ', user, ' at ', pixabay);
+
+    const icons8 = document.createElement('a');
+    icons8.href = "https://icons8.com/";
+    icons8.append('icons 8')
+
+    const link2 = document.createElement('span');
+    link2.append('All icons by ', icons8);
+
+    nav.append(aGithub,link1, link2);
+
+    fr.append(nav);
 
     return fr;
 }
